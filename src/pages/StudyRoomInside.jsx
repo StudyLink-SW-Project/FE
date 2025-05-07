e8d4317ecbf51fad3ba792e8d8600e62b52eb416
lehojun <hojunchris@naver.com>
2025-05-07 12:43:07 +0900

    [FEAT] api 호출 url 수정

--- src/pages/StudyRoomInside.jsx
+++ src/pages/StudyRoomInside.jsx
@@ -39,7 +39,7 @@ export default function StudyRoomInside() {
 
     (async () => {
       try {
-        const res = await fetch(`${APP_SERVER}/api/v1/video/token`, {
+        const res = await fetch(`${APP_SERVER}api/v1/video/token`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ roomName: id, participantName }),
