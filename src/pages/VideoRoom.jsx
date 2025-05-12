341b9b2fdbec52a35665ed6c33345b089a206748
lehojun <hojunchris@naver.com>
2025-05-12 11:13:17 +0900

    [FEAT] Openvidu URL 수정

--- src/pages/VideoRoom.jsx
+++ src/pages/VideoRoom.jsx
@@ -29,7 +29,7 @@ import AudioComponent from "../components/AudioComponent";
         if (window.location.hostname === "localhost") {
             LIVEKIT_URL = "ws://localhost:7880/";
         } else {
-            LIVEKIT_URL = "wss://" + window.location.hostname + ":443/";
+            LIVEKIT_URL = "wss://" + window.location.hostname + ":8443/";
         }
     }
 
