41dd606ebd3bdd9867cc1c5ec14e086ec6f4925a
yunjae999 <154576233+yunjae999@users.noreply.github.com>
2025-05-28 15:13:28 +0900

    [FIX] 비디오 수정

--- src/pages/StudyRoomInside.jsx
+++ src/pages/StudyRoomInside.jsx
@@ -286,7 +286,7 @@ export default function StudyRoomInside() {
         </div> */}
         {/* 비디오 컨퍼런스 그리드 */}
         <div className="flex-1 p-4 overflow-auto">
-          <VideoConference
+          <VideoTrack
           />
         </div>
 
