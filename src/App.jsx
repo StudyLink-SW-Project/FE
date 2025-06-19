138772539ce35fbaf9931202db0969a9991c1a29
yunjae999 <154576233+yunjae999@users.noreply.github.com>
2025-06-19 11:07:05 +0900

    [FIX] 오류해결

--- src/App.jsx
+++ src/App.jsx
@@ -13,45 +13,42 @@ import SignupPage from "./pages/SignupPage";
 import StudyRoomEntry from "./pages/StudyRoomEntry";
 import QuestionDetail from "./pages/QuestionDetail";
 import OAuth2Callback from "./pages/OAuth2Callback";
-import { StudyProvider } from './contexts/StudyContext';
 
 export default function App() {
   return (
     <>
       <ToastContainer position="top-center" />
-      <StudyProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/study-room" element={
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/study-room" element={
+          <ProtectedRoute>
+            <StudyRoom />
+          </ProtectedRoute>
+        } />
+        <Route path="/questions" element={
+          <ProtectedRoute>
+            <Questions />
+          </ProtectedRoute>
+        } />
+        <Route path="/my-questions" element={
+          <ProtectedRoute>
+            <MyQuestions />
+          </ProtectedRoute>
+        } />
+        <Route path="/questions/:id" element={<QuestionDetail />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/study-room/:id"
+          element={
             <ProtectedRoute>
-              <StudyRoom />
+              <StudyRoomEntry />
             </ProtectedRoute>
-          } />
-          <Route path="/questions" element={
-            <ProtectedRoute>
-              <Questions />
-            </ProtectedRoute>
-          } />
-          <Route path="/my-questions" element={
-            <ProtectedRoute>
-              <MyQuestions />
-            </ProtectedRoute>
-          } />
-          <Route path="/questions/:id" element={<QuestionDetail />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route
-            path="/study-room/:id"
-            element={
-              <ProtectedRoute>
-                <StudyRoomEntry />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/login/oauth2/code/:provider" element={<OAuth2Callback />} />      
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </StudyProvider>
+          }
+        />
+        <Route path="/login/oauth2/code/:provider" element={<OAuth2Callback />} />      
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </>
   );
 }
